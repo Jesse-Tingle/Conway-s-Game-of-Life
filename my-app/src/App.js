9b2b59bf450bbfc152709308206c13e084dbd672
Jesse-Tingle <jtingle0@gmail.com>
2020-09-28 20:06:12 -0400

    add generation counter and faster and slower buttons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,10 @@ import React, { useState, useCallback, useRef } from "react";
 import produce from "immer";
 import "./App.css";
 
-const numRows = 50;
+const numRows = 40;
 const numCols = 50;
+let generation = 0;
+let timer = 1000;
 
 const operations = [
 	[0, 1],
@@ -38,7 +40,7 @@ function App() {
 		if (!runningRef.current) {
 			return;
 		}
-
+		generation++;
 		setGrid((g) => {
 			return produce(g, (gridCopy) => {
 				for (let i = 0; i < numRows; i++) {
@@ -62,70 +64,124 @@ function App() {
 			});
 		});
 
-		setTimeout(runSimulation, 1000);
+		setTimeout(runSimulation, timer);
 	}, []);
 
 	return (
 		<div className="App">
-			<div className="buttons">
-				<button
-					onClick={() => {
-						setRunning(!running);
-						if (!running) {
-							runningRef.current = true;
-							runSimulation();
-						}
-					}}
-				>
-					{running ? "Stop" : "Start"}
-				</button>
-				<button
-					onClick={() => {
-						setGrid(generateEmptyGrid());
-					}}
-				>
-					Clear
-				</button>
-				<button
-					onClick={() => {
-						const rows = [];
-						for (let i = 0; i < numRows; i++) {
-							rows.push(
-								Array.from(Array(numCols), () => (Math.random() > 0.9 ? 1 : 0))
-							);
-						}
-
-						setGrid(rows);
-					}}
-				>
-					Randomize
-				</button>
+			<div className="header">
+				<h1>Conway's Game of Life</h1>
 			</div>
-			<div
-				style={{
-					display: "grid",
-					gridTemplateColumns: `repeat(${numCols}, 20px)`,
-				}}
-			>
-				{grid.map((rows, i) =>
-					rows.map((col, k) => (
-						<div
-							key={`${i}-${k}`}
+
+			<div className="game-container">
+				<div className="buttons-and-about">
+					<div className="buttons">
+						<button
 							onClick={() => {
-								const newGrid = produce(grid, (gridCopy) => {
-									gridCopy[i][k] = grid[i][k] ? 0 : 1;
-								});
-								setGrid(newGrid);
+								setRunning(!running);
+								if (!running) {
+									runningRef.current = true;
+									runSimulation();
+								}
 							}}
-							style={{
-								width: 20,
-								height: 20,
-								backgroundColor: grid[i][k] ? "aqua" : undefined,
-								border: "solid 1px black",
+						>
+							{running ? "Stop" : "Start"}
+						</button>
+						<button
+							onClick={() => {
+								setGrid(generateEmptyGrid());
 							}}
-						></div>
-					))
-				)}
+						>
+							Clear
+						</button>
+						<button
+							onClick={() => {
+								const rows = [];
+								for (let i = 0; i < numRows; i++) {
+									rows.push(
+										Array.from(Array(numCols), () =>
+											Math.random() > 0.9 ? 1 : 0
+										)
+									);
+								}
+
+								setGrid(rows);
+							}}
+						>
+							Randomize
+						</button>
+						<div className="speed">
+							<button
+								onClick={() => {
+									timer -= 100;
+								}}
+							>
+								faster
+							</button>
+							<button
+								onClick={() => {
+									timer += 100;
+								}}
+							>
+								slower
+							</button>
+						</div>
+					</div>
+					<div className="generation">Generation: {`${generation}`}</div>
+					<div className="about">
+						<h2>Rules of the Game:</h2>
+						<ol>
+							<li>
+								Any live cell with fewer than two live neighbours dies, as if by
+								underpopulation.
+							</li>
+							<li>
+								Any live cell with two or three live neighbours lives on to the
+								next generation.
+							</li>
+							<li>
+								Any live cell with more than three live neighbours dies, as if
+								by overpopulation.
+							</li>
+							<li>
+								Any dead cell with exactly three live neighbours becomes a live
+								cell, as if by reproduction.
+							</li>
+						</ol>
+
+						{/* <h2>Turing Complete</h2>
+				<p>
+					A machine or language is considered Turning complete if when given
+					enough time and memory are able to compute any program.
+				</p> */}
+					</div>
+				</div>
+				<div
+					style={{
+						display: "grid",
+						gridTemplateColumns: `repeat(${numCols}, 20px)`,
+					}}
+				>
+					{grid.map((rows, i) =>
+						rows.map((col, k) => (
+							<div
+								key={`${i}-${k}`}
+								onClick={() => {
+									const newGrid = produce(grid, (gridCopy) => {
+										gridCopy[i][k] = grid[i][k] ? 0 : 1;
+									});
+									setGrid(newGrid);
+								}}
+								style={{
+									width: 20,
+									height: 20,
+									backgroundColor: grid[i][k] ? "aqua" : undefined,
+									border: "solid 1px black",
+								}}
+							></div>
+						))
+					)}
+				</div>
 			</div>
 		</div>
 	);
